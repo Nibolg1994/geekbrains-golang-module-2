946434f6988c80f5d5f5e85dffab835bcd2deb61
Nibolg1994 <globin.anvi@gmail.com>
2021-04-04 14:16:56 +0300

    lesson 2

--- lesson-8/file-scanner.go
+++ lesson-8/file-scanner.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"io/ioutil"
-	"log"
 	"os"
 	"runtime"
 	"sync"
+	"time"
 )
 
 type File struct {
@@ -18,6 +20,10 @@ type File struct {
 var fileList sync.Map
 
 func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	logger = logger.With(zap.String("goos", runtime.GOOS))
+
 	dir := flag.String("dir", "./", "Scan directory")
 	deleteFile := flag.Bool("delete", false, "Delete duplicates flag")
 	flag.Parse()
@@ -28,26 +34,60 @@ func main() {
 }
 
 func scanner(workers chan struct{}, dir string, deleteFile bool) chan struct{} {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.Level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+	logger, _ := config.Build()
+	logger = logger.With(zap.String("goos", runtime.GOOS))
+	defer logger.Sync()
+
 	workers <- struct{}{}
 	files, err := ioutil.ReadDir(dir)
 	chanels := make([]chan struct{}, 0, len(files))
 	result := make(chan struct{})
 
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err.Error())
 	}
+
 	go func() {
-		defer func() { result <- struct{}{} }()
-		defer func() { <-workers }()
+		defer func() {
+			logger.With(
+				zap.String("time", time.Now().String()),
+				zap.String("dir", dir),
+			).Debug("thread finished")
+			<-workers
+			result <- struct{}{}
+		}()
 
 		for _, f := range files {
 			if f.IsDir() {
-				chanels = append(chanels, scanner(workers, dir+string(os.PathSeparator)+f.Name(), deleteFile))
+				dirName := dir + string(os.PathSeparator) + f.Name()
+				logger.With(
+					zap.String("time", time.Now().String()),
+					zap.String("dir", dirName),
+				).Debug("start new thread")
+				chanels = append(chanels, scanner(workers, dirName, deleteFile))
+
 			} else {
 				file := File{name: f.Name(), size: f.Size()}
 				_, ok := fileList.Load(file)
 				if ok {
-					err = os.Remove(dir + string(os.PathSeparator) + f.Name())
+					filename := dir + string(os.PathSeparator) + f.Name()
+					if deleteFile {
+						err = os.Remove(filename)
+						if err == nil {
+							logger.With(
+								zap.String("time", time.Now().String()),
+								zap.String("file", filename),
+							).Info("file deleted")
+						} else {
+							logger.With(
+								zap.String("time", time.Now().String()),
+								zap.String("file", filename),
+							).Warn("file is not deleted")
+						}
+					}
 					fmt.Println(f.Name())
 				} else {
 					fileList.Store(file, struct{}{})
